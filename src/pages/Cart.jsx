c572e80aeada116c0ba3153a1f966f7bbfad04e6
Diti2604 <ditiboyboy@gmail.com>
2024-06-10 16:00:30 +0200

    express change

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
 
   const checkout = async () => {
     try {
-      const response = await fetch("https://localhost:4000/checkout", {
+      const response = await fetch("http://localhost:4000/checkout", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

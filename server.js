610b5f190cef9b0b1a6d48a59b21e2a897daf26b
Diti2604 <ditiboyboy@gmail.com>
2024-06-10 16:35:53 +0200

    last change

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-const express = require("express");
 const stripe = require("stripe")("sk_test_51O7w6WKs6k4Ri2v4qQEuNLqoaaLMdUezwYmUAZvtq9CUEVXSA2Yr5azIudh7lLbjKsnWxznoNGFQjBHEhP7ngKQC00uQoJh9EO");
-const cors = require("cors");
 
-const app = express();
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
 
-app.use(cors());
-app.use(express.static("public"));
-app.use(express.json());
-
-app.post("/checkout", async (req, res) => {
-  console.log(req.body);
   const items = req.body.items;
   let lineItems = [];
   items.forEach((item) => {
     lineItems.push({
       price: item.price,
       quantity: item.quantity,
-      
     });
   });
 
@@ -28,12 +22,8 @@ app.post("/checkout", async (req, res) => {
       cancel_url: "https://library-ecom-app-react.vercel.app/cancel",
     });
 
-    res.send(JSON.stringify({ url: session.url }));
+    res.json({ url: session.url });
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
-});
-
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
-});
+};
